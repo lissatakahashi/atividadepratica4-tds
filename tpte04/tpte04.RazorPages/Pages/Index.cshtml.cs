1564d5f16b89958539e7aa2763f8793bcfc5b7ad
Lissa Takahashi <99764365+lissatakahashi@users.noreply.github.com>
2023-09-10 13:37:29 -0300

    total de cursos e alunos

--- tpte04/tpte04.RazorPages/Pages/Index.cshtml.cs
+++ tpte04/tpte04.RazorPages/Pages/Index.cshtml.cs
@@ -1,16 +1,26 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using tpte04.RazorPages.Data;
 
 namespace tpte04.RazorPages.Pages
 {
     public class Index : PageModel
-    {
 
-        public Index()
+    {
+    private readonly AppDbContext _context;
+        public Index(AppDbContext context)
         {
+            _context = context;
         }
+
+        public int TotalDeCursos { get; set; }
+        public int TotalDeAlunos { get; set; }
+        public int CursoComMaisAlunos { get; set; }
+
 
         public void OnGet()
         {
+            TotalDeCursos = _context.Cursos!.Count();
+            TotalDeAlunos = _context.Alunos!.Count();
         }
     }
 }
